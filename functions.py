3752fe0004696a250d17bea24a74c8ae14afae41
Sergey Kamskov <sk1999@inbox.ru>
2022-02-28 15:28:47 +0300

    Data filtering and reloading implemented

---
+++ functions.py
@@ -0,0 +1,89 @@
+import numpy as np
+import pandas as pd
+
+
+class Event:
+    def __init__(self, id, dataframe):
+        self.df = dataframe.loc[dataframe["id"] == id]
+        self.id = id
+        self.std = 0
+        self.avg = 0
+
+    def filter(self, sigma=2):
+        self.avg = self.df["temperatureAvg"].mean()
+        self.std = self.df.std(ddof=0)["temperatureAvg"]
+        self.df = self.df.loc[(self.avg - sigma * self.std < self.df['temperatureAvg']) \
+                              & (self.df['temperatureAvg'] < self.avg + sigma * self.std)]
+
+
+class Model:
+    def __init__(self, dataframe_input):
+        self.df_all = None
+        self.df_stat = None
+        self.df = dataframe_input
+        self.events = []
+        self.load()
+
+
+
+    def show(self, index):
+        return self.events[index].df
+
+    def filter(self, sigma=2):
+        for item in self.events:
+            item.filter(sigma)
+        self.concat()
+
+    def concat(self):
+        self.concat_all()
+        self.concat_stat()
+
+    def concat_all(self):
+        self.df_all = self.events[0].df
+        for i in range(1, len(self.events)):
+            self.df_all = pd.concat([self.df_all, self.events[i].df])
+
+    def concat_stat(self):
+        var_mean = []
+        var_std = []
+        var_id = []
+        for item in self.events:
+            var_mean.append(item.avg)
+            var_std.append(item.std)
+            var_id.append(item.id)
+        self.df_stat = pd.DataFrame({'id': var_id, 'temperatureAvg': var_mean, 'temperatureSdv': var_std})
+
+    def load(self, dataframe = None):
+        if dataframe is not None:
+            self.df = dataframe
+        self.events = []
+        for id_name in self.df["id"].unique():
+            self.events.append(Event(id_name, self.df))
+        self.concat()
+
+
+class Import:
+    def do_import(data):
+        data_list = data.split("\r")
+        data_list = list(map(lambda x: x[2:], data_list))
+        data_list = data_list[1:-1]
+        data_list = list(map(lambda x: x.split(' '), data_list))
+        id = np.array(list(map(lambda x: x[2], data_list)))
+        dist = np.array(list(map(lambda x: x[5], data_list)))
+        t = np.array(list(map(lambda x: x[8], data_list)))
+        ts = np.array(list(map(lambda x: x[11], data_list)))
+        x = np.array(list(map(lambda x: x[14], data_list)))
+        y = np.array(list(map(lambda x: x[17], data_list)))
+        w = np.array(list(map(lambda x: x[20], data_list)))
+        h = np.array(list(map(lambda x: x[23], data_list)))
+        strange_thing = np.array(list(map(lambda x: x[24], data_list)))
+        x_ir = np.array(list(map(lambda x: x[27], data_list)))
+        y_ir = np.array(list(map(lambda x: x[30], data_list)))
+        w_ir = np.array(list(map(lambda x: x[33], data_list)))
+        h_ir = np.array(list(map(lambda x: x[36], data_list)))
+        df = pd.DataFrame({"id": id, "dist": dist, "t": t, "ts": ts, "x": x, "y": y, "w": w, "h": h, \
+                           "strange_thing": strange_thing, "x_ir": x_ir, "y_ir": y_ir, "w_ir": w_ir, "h_ir": h_ir})
+
+# %%
+
+# %%
